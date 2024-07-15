83256bb19beeea23b8e604400a92d58354b0c2b5
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-07-15 16:33:56 +0000

    wtf is that?

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tests-with-py
+
 Idk much about py, but let's try
+
+use `python ./main.py` to execute the program, for some reason, this command is executing in v. 3.09 of py, so for now, I will use `/usr/local/bin/python ./main.py`
---
+++ main.py
@@ -0,0 +1,15 @@
+import string
+
+print("hello world!")
+
+while True:
+    value = input("> ")
+    #
+    for word in value.split():
+        for char in word:
+            if char.isnumeric():
+                print("num")
+            else:
+                print("other")
+            #print(char, end='')
+        print("? " + word)
